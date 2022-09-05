fc41e0a98a1af54cd3c428bbdc86baee5c8865b8
loonhtet <gurdamanson@gmail.com>
2022-09-05 19:23:08 +0630

    5/9/22

---
+++ pricingbox/app.js
@@ -0,0 +1,23 @@
+const getcheckbox = document.getElementById("toggle-checkbox");
+const getchecklabel = document.getElementById("toggle-label");
+const getbasic = document.getElementById("basic");
+const getprof = document.getElementById("pro");
+const getmst = document.getElementById("mst");
+
+getchecklabel.addEventListener("click", () => {
+  if (getcheckbox.checked) {
+    getbasic.textContent = 10;
+    getprof.textContent = 20;
+    getmst.textContent = 30;
+
+    // or
+
+    [getbasic.textContent, getprof.textContent, getmst.textContent] = [
+      10, 20, 30,
+    ];
+  } else {
+    getbasic.textContent = 120;
+    getprof.textContent = 240;
+    getmst.textContent = 360;
+  }
+});
