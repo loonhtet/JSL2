d05dd5bc56ae889c0f973bb3cedb8c2d4117fba9
loonhtet <gurdamanson@gmail.com>
2022-09-08 15:11:13 +0630

    8/9/22

--- emailbox/app.js
+++ emailbox/app.js
@@ -10,5 +10,61 @@ getemail.addEventListener("keyup", function (e) {
 });
 
 function createemaillist(inputtext) {
-  console.log(inputtext);
+  //   console.log(inputtext);
+
+  // remove empty, empty(space)
+  //   const eitems = inputtext
+  //     .split(",")
+  //     .filter((rmempty) => rmempty.trim() !== "");
+  //   console.log(eitems);
+
+  // remove space/empty
+  const emitems = inputtext
+    .split(",")
+    .filter((reempty) => reempty.trim() !== "")
+    .map((reempty) => reempty.trim());
+  // console.log(emitems);
+
+  getemcontainer.innerHTML = "";
+
+  emitems.forEach(function (emitem) {
+    // console.log(emitem)
+
+    const setnewspan = document.createElement("span");
+    setnewspan.textContent = emitem;
+    setnewspan.classList.add("email-item");
+    // console.log(setnewspan);
+
+    getemcontainer.appendChild(setnewspan);
+  });
+}
+
+getbtn.addEventListener("click", function (e) {
+  sendemail();
+
+  e.preventDefault();
+});
+
+function sendemail() {
+  const gettxtvalue = gettextarea.value;
+  const getaddresses = document.querySelectorAll(".email-item");
+
+  // console.log(gettxtvalue);
+  // console.log(getaddresses);
+
+  var persons = [];
+
+  if (getaddresses.length > 0 && gettxtvalue) {
+    getaddresses.forEach(function (getaddress) {
+      persons.push({
+        email: getaddress.textContent,
+        message: gettxtvalue,
+      });
+    });
+
+    console.log(persons);
+  } else {
+    window.alert("Enter Message");
+    gettextarea.focus();
+  }
 }
